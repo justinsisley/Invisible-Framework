ef57620d6b87482ac86eb6c354453b2af3f7f607
Justin Sisley <manmademammal@Justins-MacBook-Pro.local>
2016-08-15 13:06:28 -0700

    Check for existence of tests before executing

--- tasks/test.js
+++ tasks/test.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const cp = require('child_process');
+const glob = require('glob');
 const escapePath = require('../utils/escapePath');
 
 // Run eslint and execute Mocha tests
@@ -19,19 +20,25 @@ const test = () => {
     process.exit(1);
   }
 
-  // Keep the output from mocha pure by catching errors thrown by execSync
-  try {
-    cp.execSync(`
-      NODE_ENV=test ${npmBin}/babel-istanbul \
-        cover ${npmBin}/_mocha -- \
-        --compilers .:${configDir}/tests/compiler.js \
-        --require ${configDir}/tests/setup.js \
-        "${cwd}/?(client|server)/**/test.js"
-    `, { stdio: 'inherit' });
-  // eslint-disable-next-line
-  } catch (err) {
-    process.exit(1);
-  }
+  // Check for existence of test files before attempting to execute
+  glob(`${cwd}/?(client|server)/**/test.js`, (error, files) => {
+    // No tests exist, we're done
+    if (!files.length) { return; }
+
+    // Keep the output from mocha pure by catching errors thrown by execSync
+    try {
+      cp.execSync(`
+        NODE_ENV=test ${npmBin}/babel-istanbul \
+          cover ${npmBin}/_mocha -- \
+          --compilers .:${configDir}/tests/compiler.js \
+          --require ${configDir}/tests/setup.js \
+          "${cwd}/?(client|server)/**/test.js"
+      `, { stdio: 'inherit' });
+    // eslint-disable-next-line
+    } catch (err) {
+      process.exit(1);
+    }
+  });
 };
 
 module.exports = test;
