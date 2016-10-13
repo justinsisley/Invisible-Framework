1b54bf1c11a8f1b88286706777de8011d8e84a0e
Justin Sisley <manmademammal@Justins-MacBook-Pro.local>
2016-10-12 17:44:44 -0700

    Fix for postinstall

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
-const escapePath = require('../utils/escapePath');
 
 const cwd = process.cwd();
 const hostPackage = path.join(cwd, './package.json');
 
 const packageRaw = fs.readFileSync(hostPackage, { encoding: 'utf8' });
 const packageJSON = JSON.parse(packageRaw);
-const iframeCmd = escapePath(path.join(__dirname, '../../../node_modules/.bin/iframe'));
+const iframeCmd = path.join(__dirname, '../../../node_modules/.bin/iframe');
 
 // If the package.json file has an _id property, IFrame is being installed as a
 // dependency, and we should run the postinstall script.
 // eslint-disable-next-line
 if (packageJSON._id) {
-  cp.execSync(`${iframeCmd} --setup`);
+  cp.execSync(`"${iframeCmd}" --setup`);
 }
