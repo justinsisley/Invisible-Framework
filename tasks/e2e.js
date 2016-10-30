d15dae21d520643fd6661f28a54145811e85ffda
Justin Sisley <manmademammal@Justins-MacBook-Pro.local>
2016-10-29 19:38:46 -0700

    e2e fixes

--- tasks/e2e.js
+++ tasks/e2e.js
@@ -6,18 +6,26 @@ const configDir = path.join(__dirname, '../config');
 const npmBin = path.join(cwd, './node_modules/.bin');
 
 const e2e = () => {
-  // Keep the output from Nightwatch pure by catching errors thrown by execSync.
-  // Always exit with 0 code to avoid NPM errors when linting fails.
-  try {
-    cp.execSync(`
-      "${npmBin}/nightwatch" \
-        --config "${configDir}/tests/e2e/config.js" || exit 0
-    `, { stdio: 'inherit' });
-  } catch (err) { // eslint-disable-line
-    process.exit(1);
-  }
+  // Give the express server a few seconds to start
+  // FIXME: there must be a better way...
+  setTimeout(() => {
+    // Keep the output from Nightwatch pure by catching errors thrown by execSync.
+    // Always exit with 0 code to avoid NPM errors when linting fails.
+    try {
+      cp.execSync(`
+        "${npmBin}/nightwatch" \
+          --config "${configDir}/tests/e2e/config.js" || exit 0
+      `, { stdio: 'inherit' });
+    } catch (err) { // eslint-disable-line
+      process.exit(1);
+    }
 
-  process.exit(0);
+    // Exit the Nightwatch process
+    process.exit(0);
+
+    // Exit the Express server's process
+    process.exit(0);
+  }, 3000);
 };
 
 module.exports = e2e;
