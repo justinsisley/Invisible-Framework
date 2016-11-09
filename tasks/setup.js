52cce134d6b2ca1dd5f81919e24ded9fa2d32ae0
Justin Sisley <manmademammal@Justins-MacBook-Pro.local>
2016-11-08 20:12:39 -0800

    Minor cleanup of setup task; using 3rd party array unique lib

--- tasks/setup.js
+++ tasks/setup.js
@@ -1,6 +1,18 @@
 const path = require('path');
 const cp = require('child_process');
 const fs = require('fs');
+const arrayUniq = require('array-uniq');
+
+const templatesDir = path.join(__dirname, '../templates');
+
+let cwd = process.cwd();
+try {
+  // When run from npm scripts, this directory will resolve
+  fs.readdirSync(path.join(cwd, '/.git'));
+} catch (err) {
+  // When we run the post-install, we need to traverse up several directories
+  cwd = path.join(cwd, '../..');
+}
 
 const exec = (command) => {
   try {
@@ -10,43 +22,16 @@ const exec = (command) => {
 
 const readFile = filepath => fs.readFileSync(filepath, { encoding: 'utf8' });
 
-function unique(array) {
-  var a = array.concat(); // eslint-disable-line
-
-  // eslint-disable-next-line
-  for (var i = 0; i < a.length; ++i) {
-    // eslint-disable-next-line
-    for (var j = i + 1; j < a.length; ++j) {
-      if (a[i] === a[j]) {
-        a.splice(j--, 1); // eslint-disable-line
-      }
-    }
-  }
-
-  return a;
-}
-
-// Basic project setup
-const setup = () => {
-  var cwd = process.cwd(); // eslint-disable-line
-
-  try {
-    // When run from npm scripts, this directory will resolve
-    fs.readdirSync(path.join(cwd, '/.git'));
-  } catch (err) {
-    // When we run the post-install, we need to traverse up several directories
-    cwd = path.join(cwd, '../..');
-  }
-
-  const templatesDir = path.join(__dirname, '../templates');
-
-  // Add pre-commit hook
+// Add pre-commit hook
+const precommit = () => {
   exec(`
     echo "#!/bin/sh" > "${cwd}/.git/hooks/pre-commit" &&
     echo "npm test" >> "${cwd}/.git/hooks/pre-commit"
   `);
+};
 
-  // Add .gitignore; modify if one exists; create if one doesn't
+// Add .gitignore; modify if one exists; create if one doesn't
+const gitignore = () => {
   try {
     const existingGitIgnoreData = readFile(`${cwd}/.gitignore`);
     const existingIgnores = existingGitIgnoreData.split('\n');
@@ -54,38 +39,46 @@ const setup = () => {
     const templateGitIgnoreData = readFile(`${templatesDir}/_gitignore`);
     const templateIgnores = templateGitIgnoreData.split('\n');
 
-    const targetIgnores = unique(templateIgnores.concat(existingIgnores));
+    const targetIgnores = arrayUniq(templateIgnores.concat(existingIgnores));
     const targetIgnoresData = targetIgnores.join('\n');
     fs.writeFileSync(`${cwd}/.gitignore`, targetIgnoresData);
   } catch (err) {
     exec(`cp "${templatesDir}/_gitignore" "${cwd}/.gitignore"`);
   }
+};
 
-  // Add .eslintrc without overwriting existing
-  // TODO: need to be able to patch existing
+// Add .eslintrc without overwriting existing
+// TODO: need to be able to patch existing
+const eslintrc = () => {
   try {
     readFile(`${cwd}/.eslintrc`);
   } catch (error) {
     exec(`cp "${templatesDir}/_eslintrc" "${cwd}/.eslintrc"`);
   }
+};
 
-  // Add .stylelintrc without overwriting existing
-  // TODO: need to be able to patch existing
+// Add .stylelintrc without overwriting existing
+// TODO: need to be able to patch existing
+const stylelintrc = () => {
   try {
     readFile(`${cwd}/.stylelintrc`);
   } catch (error) {
     exec(`cp "${templatesDir}/_stylelintrc" "${cwd}/.stylelintrc"`);
   }
+};
 
-  // Add .editorconfig without overwriting existing
-  // TODO: need to be able to patch existing
+// Add .editorconfig without overwriting existing
+// TODO: need to be able to patch existing
+const editorconfig = () => {
   try {
     readFile(`${cwd}/.editorconfig`);
   } catch (error) {
     exec(`cp "${templatesDir}/_editorconfig" "${cwd}/.editorconfig"`);
   }
+};
 
-  // Add config.js
+// Add config.js
+const configFile = () => {
   var configJson = {}; // eslint-disable-line
   // If config.js already exists, don't overwrite it
   try {
@@ -96,8 +89,10 @@ const setup = () => {
     const templateConfig = readFile(`${templatesDir}/_config.js`);
     fs.writeFileSync(`${cwd}/config.js`, templateConfig);
   }
+};
 
-  // Set up npm scripts
+// Set up npm scripts
+const npmScripts = () => {
   try {
     const packageJson = readFile(`${cwd}/package.json`);
     const parsedPackageJson = JSON.parse(packageJson);
@@ -120,6 +115,17 @@ const setup = () => {
       JSON.stringify(parsedPackageJson, null, 2)
     );
   } catch (err) {} // eslint-disable-line
+};
+
+// Basic project setup
+const setup = () => {
+  precommit();
+  gitignore();
+  eslintrc();
+  stylelintrc();
+  editorconfig();
+  configFile();
+  npmScripts();
 };
 
 module.exports = setup;
